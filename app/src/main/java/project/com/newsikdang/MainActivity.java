21b1f843fa9f22887d774a2db17a3d74c5d07d8b
SongDaHee <jilhdd@naver.com>
2018-11-09 13:32:18 +0900

    //login+main

--- app/src/main/java/project/com/newsikdang/MainActivity.java
+++ app/src/main/java/project/com/newsikdang/MainActivity.java
@@ -1,89 +1,33 @@
 package project.com.newsikdang;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvTest;
-
-    static final String API_KEY = "72676c564e6e6f6d37326c75637a4a";
-    static final String API_URL = "http://openapi.gwangjin.go.kr:8088";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvTest = findViewById(R.id.tvTest);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
 
-        new RetrieveFeedTask().execute();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-    }
-
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-
-        private Exception exception;
-
-        protected void onPreExecute() {
-            tvTest.setText("");
-        }
-
-        protected String doInBackground(Void... urls) {
-            // Do some validation here
-
-            try {
-                URL url = new URL(API_URL + "/" + API_KEY + "/xml/GwangjinFoodHygieneBizRestaurant/280/285/2018");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
             }
-            catch(Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-        protected void onPostExecute(String response) {
-            if(response == null) {
-                response = "THERE WAS AN ERROR";
             }
-            Log.i("INFO", response);
-            tvTest.setText(response);
-            // TODO: check this.exception
-            // TODO: do something with the feed
-
-//            try {
-//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-//                String requestID = object.getString("requestId");
-//                int likelihood = object.getInt("likelihood");
-//                JSONArray photos = object.getJSONArray("photos");
-//                .
-//                .
-//                .
-//                .
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-        }
+        });
     }
 }
