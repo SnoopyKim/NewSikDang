7dfe687e82022e0f04074f0ff4014273622148a5
SnoopyKim <nombu71@gmail.com>
2018-11-09 13:53:27 +0900

    add comment in index.js

--- node/functions/index.js
+++ node/functions/index.js
@@ -7,7 +7,7 @@ const schedule = require('node-schedule');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-//Get a database reference to our blog
+//Get a database reference to our project
 var db = admin.database();
 var restaurantsRef = db.ref("restaurants");
 
@@ -16,6 +16,7 @@ function saveData() {
   request('http://openapi.gwangjin.go.kr:8088/72676c564e6e6f6d37326c75637a4a/json/GwangjinFoodHygieneBizRestaurant/1/1000/2018', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // result -> restaurants data list
     var result = JSON.parse(body).GwangjinFoodHygieneBizRestaurant.row;
     var i, cnt = 0;
     var arrOk = new Array;
@@ -25,11 +26,12 @@ function saveData() {
       var cgg = result[i].CGG_CODE; //광진구 코드
       var upso_sno = result[i].UPSO_SNO; //업소일련번호(키값)
       var upso_nm = result[i].UPSO_NM; //업소명
-      upso_nm = upso_nm.replace(/\.|#|\$|\[|\]/gi, "");
+      upso_nm = upso_nm.replace(/\.|#|\$|\[|\]/gi, ""); // -> for illegal char
       var address = result[i].SITE_ADDR; //주소(지번)
       var category = result[i].SNT_UPTAE_NM; //카테고리
       var tel = result[i].UPSO_SITE_TELNO; //전화번호
 
+      // if date diff is over 100days, skip it
       var date = parseInt(result[i].BMAN_STDT); //영업자 시작일
       var date_cal = new Date(parseInt(date/10000),parseInt((date%10000)/100)-1,date%100);
       if ((today-date_cal)/(1000*60*60*24) > 100)  {
@@ -37,8 +39,11 @@ function saveData() {
         continue;
       }
 
+      // else save data into realtime database
+      // location -> /restaurants/area_code/key(snum+name)
       arrOk.push(upso_sno);
-      restaurantsRef.child(cgg).child(upso_sno+upso_nm).set({
+      var key = upso_sno+upso_nm;
+      restaurantsRef.child(cgg).child(key).set({
         snum: upso_sno,
         name: upso_nm,
         address: address,
@@ -47,12 +52,16 @@ function saveData() {
         date: date
       });
     }
+    // write log number of save data & skip data
     console.log("cnt_ok: ", arrOk.length, "\ncnt_skip", arrSkip.length);
   });
 }
 
-var event = schedule.scheduleJob('0 0 * * *', saveData);
+// set schedule to call saveData() at 0:00 every day
+var event = schedule.scheduleJob('0 14 * * *', saveData);
 
+// basic function for firebase functions
+// check it at https://us-central1-newsikdang-21cb1.cloudfunctions.net/test
 exports.test = functions.https.onRequest((request, response) => {
 
   response.send("Hello from Firebase!");
