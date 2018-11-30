db24f61ed453ae2f0b822d6461aab21fbacafae7
nari <ri7070@naver.com>
2018-11-30 18:01:01 +0900

    two fragment edit

--- app/src/main/java/project/com/newsikdang/AreaActivity.java
+++ app/src/main/java/project/com/newsikdang/AreaActivity.java
@@ -1,8 +1,11 @@
 package project.com.newsikdang;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -25,11 +28,49 @@ public class AreaActivity extends FragmentActivity implements View.OnClickListen
         a_btn1 = findViewById(R.id.a_btn1);
         a_btn1.setOnClickListener(this);
 
+        mCurrentFragmentIndex = FRAGMENT_ONE;
+        fragmentReplace(mCurrentFragmentIndex);
 
+
+    }
+
+    public void fragmentReplace(int reqNewFragmentIndex) {
+        Fragment newFragment = null; Log.d(TAG, "fragmentReplace " + reqNewFragmentIndex);
+        newFragment = getFragment(reqNewFragmentIndex);
+        // replace fragment
+        final FragmentTransaction transaction = getSupportFragmentManager() .beginTransaction();
+        transaction.replace(R.id.area_fragment, newFragment);
+        // Commit the transaction
+        transaction.commit(); }
+
+
+    private Fragment getFragment(int idx) {
+        Fragment newFragment = null;
+        switch (idx) {
+            case FRAGMENT_ONE:
+                newFragment = new map_1();
+                break;
+            case FRAGMENT_TWO:
+                newFragment = new map_2();
+                break;
+            case FRAGMENT_THREE:
+                newFragment = new map_3();
+                break;
+            default:
+                Log.d(TAG, "Unhandle case");
+                break;
+        }
+        return newFragment;
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.a_btn1:
+                mCurrentFragmentIndex = FRAGMENT_ONE;
+                fragmentReplace(mCurrentFragmentIndex);
+                a_btn1.setEnabled(false);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/project/com/newsikdang/map_1.java
@@ -0,0 +1,16 @@
+package project.com.newsikdang;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class map_1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_map_1, container, false);
+        return v;
+    }
+}
