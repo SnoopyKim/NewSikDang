22d58e092a3ceae6b42ee8773d12cf419812ed01
SongDaHee <jilhdd@naver.com>
2018-12-05 22:54:32 +0900

    #gmap 2차

--- app/src/main/java/project/com/newsikdang/Map.java
+++ app/src/main/java/project/com/newsikdang/Map.java
@@ -1,29 +1,38 @@
 package project.com.newsikdang;
 
-import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+public class Map extends Fragment
+        implements OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback {
 
-public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback {
+    public static Map newInstance() {
+        Map map = new Map();
+        return map;
+    }
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.twofragment);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.map, null, false);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment)fragmentManager
-                .findFragmentById(R.id.frag3_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        return view;
     }
 
     @Override
@@ -40,5 +49,4 @@ public class MapsActivity extends FragmentActivity
         map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
-
 }
